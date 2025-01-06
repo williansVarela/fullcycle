cb69ef117123abad713de7b62b7023e5d655e814
Willians Varela <willians.varela@gmail.com>
2025-01-05 22:41:14 -0300

    feat(terraform): add VPC and subnets configuration

---
+++ terraform/vpc.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "fullcycle-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "fullcycle-vpc"
+  }
+}
+
+
+resource "aws_subnet" "fullcycle-subnet-1" {
+  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.fullcycle-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  tags = {
+    Name = "fullcycle-subnet"
+  }
+}
+
+resource "aws_subnet" "fullcycle-subnet-2" {
+  availability_zone = "us-east-1b"
+  vpc_id            = aws_vpc.fullcycle-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "fullcycle-subnet"
+  }
+}
