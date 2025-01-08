ca0b1d62cb98c24f2b56934b3597bf98f37833ec
Willians Varela <willians.varela@gmail.com>
2025-01-07 23:00:51 -0300

    feat(terraform): add IAM role policy attachments for EKS cluster

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -29,3 +29,13 @@ resource "aws_iam_role" "cluster" {
   }
   POLICY
 }
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
