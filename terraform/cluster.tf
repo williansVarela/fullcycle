ebbb86c36cba87cff194ee00d2870393f1a676e0
Willians Varela <willians.varela@gmail.com>
2025-01-07 22:50:56 -0300

    feat(terraform): add IAM role resource for EKS cluster

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -11,3 +11,21 @@ resource "aws_security_group" "fullcycle-sg" {
     Name = "${var.prefix}-sg"
   }
 }
+
+resource "aws_iam_role" "cluster" {
+  name               = "${var.prefix}-${var.cluster_name}-role"
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "eks.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+  }
+  POLICY
+}
